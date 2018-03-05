23914de5222cf4fbe038f882237858857b24e801
infeng <fzhihao@outlook.com>
2018-03-05 10:29:38 +0800

    feat: process exit when compile error

--- src/build.ts
+++ src/build.ts
@@ -8,7 +8,7 @@ import assert from 'assert';
 import defaultBabelOptions from './defaultConfigs/babel';
 import { getRcConfig } from './getConfig';
 
-export default function build(args) {
+export default function build(args, cb) {
   const cwd = args.cwd;
 
   const webpackrcPath = path.resolve(args.cwd, args.config || '.webpackrc');
@@ -23,11 +23,7 @@ export default function build(args) {
   webpackConfig.plugins.push(
     new webpack.ProgressPlugin((percentage, msg, addInfo) => {
       const stream = process.stderr as any;
-      if (stream.isTTY && percentage < 0.71) {
-        stream.cursorTo(0);
-        stream.write(`📦  ${chalk.magenta(msg)} (${chalk.magenta(addInfo)})`);
-        stream.clearLine(1);
-      } else if (percentage === 1) {
+      if (percentage === 1) {
         console.log(chalk.green('\nwebpack: bundle build is now finished.'));
       }
     })
@@ -40,6 +36,7 @@ export default function build(args) {
         children: true,
       });
       console.log(stats.toString(buildInfo));
+      cb(true);
       return;
     }
     console.log(stats.toString({
@@ -47,5 +44,6 @@ export default function build(args) {
       children: false,
       modules: false,
     }));
+    cb();
   });
 }
